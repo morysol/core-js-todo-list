321f677b2446c1613f84dce84ac2198972ebaecb
Sergey Smirnov <cepera.cmuphob@gmail.com>
2023-07-31 09:57:31 +0300

    skele added

---
+++ main.js
@@ -0,0 +1,9 @@
+// While working on your task you should utilize all of the following:
+//      JavaScript
+// Import / export 
+// Array methods: map, reduce, filter (some of them)
+// Array spread operator
+// Regular expressions
+// Try / catch
+//      DOM API
+// document.querySelector
